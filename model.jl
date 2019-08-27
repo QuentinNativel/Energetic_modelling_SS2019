1ba74b0d085d26c37da44868f654ad769dcbc131
Jonathan Hanto <John@client-141-23-142-198.wlan.tu-berlin.de>
2019-08-27 17:03:02 +0200

    q

--- model.jl
+++ model.jl
@@ -5,11 +5,11 @@ using DataFrames, CSV
 using Plots; pyplot()
 using StatsPlots
 
-
+# Read the Excel sheets for tech, storage and timeseries
 tech_df = CSV.read(joinpath("data","technologies.csv"))
 storages_df = CSV.read(joinpath("data","storages.csv"))
 timeseries_df = CSV.read(joinpath("data","timeseries.csv"))
-
+# Creating strings for the different technologies 
 STOR = storages_df[:Storages] |> Array
 TECH = vcat(tech_df[:Technologies], STOR)
 NONSTOR = setdiff(TECH, STOR)
@@ -17,6 +17,15 @@ RES = [row[:Technologies] for row in eachrow(tech_df) if row[:Renewable] == 1]
 NONRES = [row[:Technologies] for row in eachrow(tech_df) if row[:Renewable] == 0]
 DISP = [row[:Technologies] for row in eachrow(tech_df) if row[:Dispatchable] == 1]
 NONDISP = [t for t in TECH if Symbol(t) in names(timeseries_df)]
+
+# collect Dataframes
+zip_cols(df::DataFrame, col1::Symbol, col2::Symbol) = Dict(collect(zip(df[col1],df[col2])))
+
+tech_df[:mc] = tech_df[:FuelCost] ./ tech_df[:Efficiency] .+ tech_df[:CarbonContent]*co2_price
+mc = zip_cols(tech_df, :Technologies, :mc)
+mc_stor = zip_cols(storages_df, :Storages, :MarginalCost)
+merge!(mc, mc_stor)
+HOURS = collect(1:8760)
 
 dispatch = Model(with_optimizer(Gurobi.Optimizer))
 @variables dispatch begin
